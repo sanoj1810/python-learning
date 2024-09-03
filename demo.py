a53b5e3f51d1e84480fd7af3bad5543122108913
M Sanoj <sanojappu20@gmail.com>
2024-09-03 14:55:28 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -16,3 +16,21 @@ class student :
 
 detail = student("sanoj",'arunachalam nagar',8939100547)
 detail.inform()
+
+
+
+# Defining a class
+class Car:
+    def __init__(self, color, model):
+        self.color = color  # Attribute
+        self.model = model  # Attribute
+
+    def drive(self):  # Method
+        print(f"The {self.color} {self.model} is driving.")
+
+# Creating an object of the Car class
+my_car = Car("red", "Toyota")  # Object of class Car
+
+# Accessing object attributes and methods
+print(my_car.color)  # Output: red
+my_car.drive()       # Output: The red Toyota is driving.
